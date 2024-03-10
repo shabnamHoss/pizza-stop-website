99160d61e36f27a43cec9e4645beac78a79a29f0
AmirHome <amir.email@yahoo.com>
2024-03-10 22:44:27 +0300

    send add item menu

--- dashboard.js
+++ dashboard.js
@@ -1,65 +1,75 @@
 // dashboard.js 
-$(document).ready(function() {
-    // Login Form Submission
-   
-$("#loginForm").submit(function(event) {
-    event.preventDefault();
-    
-    var username = $("#username").val();
-    var password = $("#password").val();
-    
-    $.ajax({
-        type: "POST",
-        url: "https://ict4510.herokuapp.com/api/login",
-        data: JSON.stringify({ username: username, password: password }),
-        contentType: "application/json",
-        success: function(response) {
-            // Show the logout button after successful login
-            $("#logoutNavItem").show();
-            
-            // Store user data in session storage
-            sessionStorage.setItem("api_key", JSON.stringify(response.user.api_key));
-            sessionStorage.setItem("token", JSON.stringify(response.user.token));
-            
-            // Hide the login form and show the menu form
-            $("#loginForm").hide();
-            $("#menuForm").show();
-        },
-        error: function(xhr, status, error) {
-            alert("Login failed. Please try again.");
-        }
-    });
-});
+$(document).ready(function () {
 
-    
+    // If token is stored in session storage, show the menu form
+    if (sessionStorage.getItem("user")) {
+        $("#menuForm").show();
+        $("#loginForm").hide();
+        $("#logoutNavItem").show();
+    } else {
+        $("#menuForm").hide();
+        $("#loginForm").show();
+    }
+
+    $("#loginForm").submit(function (event) {
+        event.preventDefault();
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        $.ajax({
+            type: "POST",
+            url: "https://ict4510.herokuapp.com/api/login",
+            data: JSON.stringify({ username: username, password: password }),
+            contentType: "application/json",
+            success: function (response) {               
+
+                // Store user data in session storage
+                sessionStorage.setItem("user", JSON.stringify(response.user));
+
+                // Hide the login form and show the menu form
+                $("#logoutNavItem").show();
+                $("#loginForm").hide();
+                $("#menuForm").show();
+            },
+            error: function (xhr, status, error) {
+                alert("Login failed. Please try again.");
+            }
+        });
+    });
+
+
     // Logout Process
-    $("#logoutButton").click(function() {
+    $("#logoutButton").click(function () {
         sessionStorage.removeItem("user");
         $("#menuForm").hide();
         $("#loginForm").show();
+        $("#logoutNavItem").show();
     });
-    
+
     // Menu Item Form Submission
-    $("#menuItemForm").submit(function(event) {
+    $("#menuItemForm").submit(function (event) {
         event.preventDefault();
-        
+
         var user = JSON.parse(sessionStorage.getItem("user"));
+
         var item = $("#item").val();
         var description = $("#description").val();
         var price = $("#price").val();
-        
+
         $.ajax({
             type: "POST",
             url: "https://ict4510.herokuapp.com/api/menus?api_key=" + user.api_key,
             data: JSON.stringify({ item: item, description: description, price: price }),
             headers: {
-                "x-access-token": user.session_token
+                "x-access-token": user.token
             },
             contentType: "application/json",
-            success: function(response) {
-                alert("Menu item added successfully!");
+            success: function (response) {
+                alert(`${response.message}!`);
+                console.log(response.message);
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 alert("Failed to add menu item. Please try again.");
             }
         });
